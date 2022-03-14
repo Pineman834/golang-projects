fd100660ac84fc89a7f026b87d05deaf9492053a
Mimmo Baluyut <mimmo.baluyut@icloud.com>
2022-03-14 19:09:12 -0400

    Base for create user

--- GODATA/main.go
+++ GODATA/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
@@ -48,29 +49,93 @@ func main() {
 	// }
 	// fmt.Printf("User found: %v\n", users)
 
+	scanner := bufio.NewScanner(os.Stdin)
+
 	fmt.Println("Login(1) or create user(2)?")
 	fmt.Print("| => ")
-	var choice int
-	fmt.Scanln(&choice)
-	if choice == 1 {
+	// var choice int
+	scanner.Scan()
+	choice := scanner.Text()
+	// fmt.Scanln(&choice)
+	if choice == "1" {
 		fmt.Println("Enter Username:")
 		fmt.Print("| => ")
-		var username string
-		fmt.Scanln(&username)
+		// var username string
+		// fmt.Scanln(&username)
+		scanner.Scan()
+		username := scanner.Text()
 
 		fmt.Println("Enter Password:")
 		fmt.Print("| => ")
-		var password string
-		fmt.Scanln(&password)
+		// var password string
+		// fmt.Scanln(&password)
+		scanner.Scan()
+		password := scanner.Text()
 
 		if login(username, password) != nil {
 			fmt.Println("Login success")
+			maininterface()
 		} else {
 			fmt.Println("Login failed")
 		}
-	} else if choice == 2 {
-		// TODO: Create user
+	} else if choice == "2" {
+		fmt.Println("Enter Name:")
+		fmt.Print("| => ")
+		// var Name string
+		// fmt.Scanln(&Name)
+		scanner.Scan()
+		Name := scanner.Text()
+
+		fmt.Println("Enter Username:")
+		fmt.Print("| => ")
+		// var Username string
+		// fmt.Scanln(&Username)
+		scanner.Scan()
+		Username := scanner.Text()
+
+		fmt.Println("Enter Email:")
+		fmt.Print("| => ")
+		// var Email string
+		// fmt.Scanln(&Email)
+		scanner.Scan()
+		Email := scanner.Text()
+
+		fmt.Println("Enter Password:")
+		fmt.Print("| => ")
+		// var Password string
+		// fmt.Scanln(&Password)
+		scanner.Scan()
+		Password := scanner.Text()
+
+		usrID, err := createUser(User{
+			Name:     Name,
+			Username: Username,
+			Email:    Email,
+			Password: Password,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID of added User: %v\n", usrID)
+		if login(Username, Password) != nil {
+			fmt.Println("Sign Up and Login success")
+			maininterface()
+		} else {
+			fmt.Println("Sign Up and Login failed")
+		}
 	}
+}
+
+func createUser(usr User) (int64, error) {
+	result, err := db.Exec("INSERT INTO user (name, username, email, password) VALUES (?, ?, ?, ?)", usr.Name, usr.Username, usr.Email, usr.Password)
+	if err != nil {
+		return 0, fmt.Errorf("createUser: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("createUser: %v", err)
+	}
+	return id, nil
 }
 
 func login(liveusername string, livepassword string) []User {
@@ -92,6 +157,10 @@ func login(liveusername string, livepassword string) []User {
 		return nil
 	}
 	return users
+}
+
+func maininterface() {
+
 }
 
 // func UserByName(name string) ([]User, error) {
